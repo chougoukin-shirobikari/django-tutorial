bf80f463036d7a27764228bcc4488538f6ca7ef8
mizuki <banana0822hairuyo@gmail.com>
2023-10-12 22:12:25 +0900

    2023/10/12 22:12

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('bbs/', include('bbs.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
